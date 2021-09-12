8e8756d7c9c8bb8d910a6326f9987f2a18ef40a1
lurkaround <rhelon@gmail.com>
2021-09-12 13:41:11 +0200

    added nav component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <div id="app">
+  <div class="main">
+    <Navigation class="navigation"></Navigation>
     <router-view />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-
 import db from './firebase/firebaseinit';
+import Navigation from './components/Navigation';
 
 export default {
   name: 'App',
+  components: {
+    Navigation,
+  },
   data() {
     return {
       APIkey: process.env.VUE_APP_APIKEY,
-      city: 'Berlin',
       cities: [],
     };
   },
@@ -27,18 +30,31 @@ export default {
 
       firebaseDB.onSnapshot((snap) => {
         snap.docChanges().forEach(async (doc) => {
-          console.log(doc);
+          if (doc.type === 'added') {
+            try {
+              const res = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${
+                  doc.doc.data().city
+                }&appid=${this.APIkey}`
+              );
+              const data = res.data;
+              firebaseDB
+                .doc(doc.doc.id)
+                .update({
+                  currentWeather: data,
+                })
+                .then(() => {
+                  this.cities.push(doc.doc.data());
+                })
+                .then(() => {
+                  console.log(this.cities);
+                });
+            } catch (err) {
+              console.error(err);
+            }
+          }
         });
       });
-    },
-    getCurrentWeather() {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.APIkey}`
-        )
-        .then((res) => {
-          console.log(res.data);
-        });
     },
   },
 };
@@ -50,5 +66,21 @@ export default {
   padding: 0;
   box-sizing: border-box;
   font-family: 'Quicksand', sans-serif;
+}
+
+.main {
+  height: 100vh;
+  .navigation {
+    z-index: 99;
+    position: fixed;
+    max-width: 1024px;
+    width: 100%;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.6);
+  }
+
+  .container {
+    padding: 0 20px;
+  }
 }
 </style>
